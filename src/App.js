587f38102eb13c64f41b3ffe9f637ffe7c119a5f
Rithik <rithu0644@gmail.com>
2022-11-26 15:04:38 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-import InputComponent from "./Components/InputComponent";
+import ColorChanger from "./Components/ColorChanger";
 
 export default function App() {
-
-  const inputList = [{ type: "text", label: "Enter your username", placeholder: "your Username", datatestid: 'username' },
-  { type: "email", label: "Enter your email", placeholder: "Your Email", datatestid: 'email' },
-  { type: "password", label: "Enter your password", placeholder: "your Password", datatestid: 'password' }]
 
   return (
     <>
       <h1>RITHIK RAJ K S    😎</h1>
-      <InputComponent prop={inputList} />
+      <ColorChanger />
     </>
   );
 }
---
+++ src/Components/ColorChanger.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import "../Styles/ColorChanger.css";
+import { Box, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
+
+function ColorChanger() {
+
+    const [color, setColor] = useState("white");
+    const handleColor = (event) => {
+        setColor(event.target.value);
+    }
+
+    return (
+        <Box className='box'
+            backgroundColor={color}
+            sx={{
+                width: 350,
+                height: 450,
+                borderRadius: 25
+            }}
+        >
+            <div className='conts'>
+                <h3>The Displayed color is</h3>
+                <h4>{color}</h4>
+                <FormControl>
+                    <InputLabel id='sel'>Color</InputLabel>
+                    <Select id={color} onChange={handleColor} label="Select" labelId='sel' sx={{ width: 200 }}>
+                        <MenuItem value={'lime'}>Lime</MenuItem>
+                        <MenuItem value={'lavender'}>Lavender</MenuItem>
+                        <MenuItem value={'crimson'}>Crimson</MenuItem>
+                        <MenuItem value={'darkblue'}>Darkblue</MenuItem>
+                        <MenuItem value={'teal'}>Teal</MenuItem>
+                        <MenuItem value={'rebeccapurple'}>Rebecca Purple</MenuItem>
+                        <MenuItem value={'ghostwhite'}>Ghost White</MenuItem>
+                        <MenuItem value={'aquamarine'}>Aqua Marine</MenuItem>
+                        <MenuItem value={'aliceblue'}>Alice Blue</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+        </Box>
+    )
+}
+
+export default ColorChanger;
